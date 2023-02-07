5c62123a2f7caef17b10f3c037e57e4b95976775
yogesh.amrutkar <yogesh.amrutkar@PLRENT-PF3BNKP7>
2023-02-07 12:19:35 +0530

    Commited new modification

--- Roman/Program.cs
+++ Roman/Program.cs
@@ -12,8 +12,10 @@ internal class Program
         Console.WriteLine("Please Enter Arabic Number Between 1 to 3999\nOther Number To Exit");
         string? input = Console.ReadLine();
         bool isNumberInput = int.TryParse(input, out int num);
-        if(!isNumberInput || num > 3999)
-            Console.WriteLine("You Enter Invalid Roman Number");
+        if (!isNumberInput || num == 0 || num > 3999)
+        {
+            Console.WriteLine("Invalid Input, Program terminted ...");
+        }
         while (isNumberInput && num > 0)
         {
             if (num == 0 || num > 3999) 
@@ -23,12 +25,14 @@ internal class Program
             Console.WriteLine("Result1 without Loop => {0}", romanResult);
 
             romanResult = obj2.ConvertArabicNumberToRoman(num);
-            Console.WriteLine("Result2 with Loop => {0}", romanResult);
+            Console.WriteLine("Result2 with Loop => {0}\n", romanResult);
 
             input = Console.ReadLine();
             isNumberInput = int.TryParse(input, out int num1);
-            if (!isNumberInput || num > 3999)
-                Console.WriteLine("You Enter Invalid Roman Number");
+            if (!isNumberInput || num1 == 0 || num1 > 3999)
+            {
+                Console.WriteLine("Invalid Input, Program terminted ...");
+            }
             num = num1;
         }
         Console.ReadLine();
